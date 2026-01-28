72bb08528b7c2f87d5089af818bf9af2b3638002
bm7273 <ut22994@bristol.ac.uk>
2026-01-28 13:41:24 +0000

    .

--- data_factory/esa_data_loader.py
+++ data_factory/esa_data_loader.py
@@ -124,7 +124,7 @@ def get_esa_loader(csv_path,
                    step=1,
                    target_channels=None,
                    mode='train',
-                   num_workers=0):
+                   num_workers=4):
     """
     Create DataLoader for ESA data
     
